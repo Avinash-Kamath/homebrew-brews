568dd56a1f6b7843f4ea42d0e630f31d36d7fc5f
avinash <avinash.kamath@scalekit.com>
2025-03-02 17:19:21 +0530

    update brew

--- Formula/scim-sim.rb
+++ Formula/scim-sim.rb
@@ -6,11 +6,15 @@ class ScimSim < Formula
   url "https://files.pythonhosted.org/packages/1f/2d/1f00fbc4723df7fe9115d1c6d1d61f9bd3cd7e7def8380c60122d4824646/scim_sim-0.0.1.tar.gz"
   sha256 "066399b0adf774f2ee9f8ddbdb05f46eb47876391bd9c38e2a155c9d5f201504"
   license "MIT"
+  version "0.0.56"
 
   depends_on "python"
 
   def install
-      virtualenv_install_with_resources
+    ohai "Installation of scim-sim version #{version} complete!"
+    virtualenv_create(libexec, "python3")
+    system libexec/"bin/pip", "install", buildpath
+    generate_completions_from_executable(libexec/"bin/scim-sim", "--help")
   end
 
   test do
