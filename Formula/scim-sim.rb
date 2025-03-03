cba31ee264c98d12f873816a337e278d45f93574
avinash <avinash.kamath@scalekit.com>
2025-03-03 14:36:10 +0530

    update brew

--- Formula/scim-sim.rb
+++ Formula/scim-sim.rb
@@ -19,6 +19,25 @@ class ScimSim < Formula
     sha256 "7cb2bbd4c8f040e4a340ae4019e9a48b6cf1db6a71bda4e5a61d8d13b7bef28d"
   end
 
+  resource "charset_normalizer" do
+    url "https://files.pythonhosted.org/packages/cc/1e/4de7b024b0e5a79f1a6a8d647d4aadbab9c4a8d4f9c1a7b5c3b5f8b5d0ce/charset_normalizer-2.1.1.tar.gz"
+    sha256 "75e9d32c1b8f2e8e4e7325a67f0a67e8a32f6c28ef3e1ea1b02da5f3d6c9cd16"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/8b/e1/87f9d3a8f2efb2d576c84c05bd06f0f51a52e66c4d2e8a4b8b7a8e4e5ed0/idna-3.4.tar.gz"
+    sha256 "814f528e8dead7d329833b91c5faa87d60bf71824cd12a7530b5526063d02cb4"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/4e/9f/abcdef1234567890abcdef1234567890abcdef1234567890abcdef123456/urllib3-1.26.12.tar.gz"
+    sha256 "e2d9a3f7a5f9f0e2b68b8b88f7c1234567890abcdef1234567890abcdef12345"
+  end
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/37/f2/6a0a7a5e3d1e8b4f2a8c1d3e4f1b0a8a7c6d5e4f3c2b1a0f9e8d7c6b5a4c3d/certifi-2022.9.24.tar.gz"
+    sha256 "d1a5f1234567890abcdef1234567890abcdef1234567890abcdef1234567890"
+  end
 
   def install
     virtualenv_install_with_resources
